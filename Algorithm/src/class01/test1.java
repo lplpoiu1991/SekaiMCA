e3f53fee8d487168770b28ddebc7e0fa07f2e8d6
597270955@qq.com <597270955@qq.com>
2021-12-12 00:02:39 +0800

    Changes

--- Algorithm/src/class01/test1.java
+++ Algorithm/src/class01/test1.java
@@ -1,13 +1,32 @@
 package class01;
 
-public class test1 {
-    public static void print(int num){
+import javax.swing.*;
 
-    };
+public class test1 {
+    public static void print(int num) {
+        for (int i = 31; i >= 0; i--) {
+            //创造每位的数字  进行与运算，判断是否为1
+            System.out.print((num & (1 << i)) == 0 ? "0" : "1");
+
+        }
+        System.out.println("");
+    }
+
 
 
     public static void main(String[] args) {
-        int num = 1232131;
+        int num = 2;
+        print(num);
+
+        System.out.println(2<<2);
+
+        System.out.println(Integer.MAX_VALUE);
+
+        print(Integer.MAX_VALUE);
+
+        System.out.println(Integer.MIN_VALUE);
+
+        print(Integer.MIN_VALUE);
 
     }
 }
